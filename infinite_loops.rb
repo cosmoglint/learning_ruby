8a8a8327a7e72c9c76041272ffdf00e473482cb4
thedoodler <ramprakashsmail@gmail.com>
2020-10-02 13:42:57 +0530

    infinite loops code

---
+++ infinite_loops.rb
@@ -0,0 +1,14 @@
+#syntax is loop do
+
+ender = 15
+i = 0
+
+loop do
+  puts i
+  i+=1
+  break if (i>=ender) 
+
+end
+
+  
+
---
+++ unless_operator.rb
@@ -0,0 +1,7 @@
+# unless is equivalent to if not
+
+x = 20
+
+(y = "x is lesser than 50") unless (x>50)
+
+puts y
