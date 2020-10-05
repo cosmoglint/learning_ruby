68daa80172bd6a25214630e802f6552b19fc918b
thedoodler <ramprakashsmail@gmail.com>
2020-10-05 16:46:36 +0530

    here documents

---
+++ here_docs.rb
@@ -0,0 +1,11 @@
+# here documents are used for multiline and complex strings
+
+# begin with <<- ends with delimiting word
+
+thestr = <<-END
+Hi there, I can go 
+on and 
+on until I say
+END
+
+puts thestr
