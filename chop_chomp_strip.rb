404fc048106b1e8853c7f7d96bbf8e0357c37788
thedoodler <ramprakashsmail@gmail.com>
2020-10-05 19:58:04 +0530

    chop chomp and strip

---
+++ chop_chomp_strip.rb
@@ -0,0 +1,11 @@
+# chomp bites off everything after the character, if no argument is given then it chomps off return and newline
+
+#chop just removes last character
+
+#strip removes trailing and leading tabs spaces and newlines
+
+puts "Hello World!  \r\n".chomp("o")
+
+puts "Hello World!".chomp("orld!")
+
+puts "this sis a string".chop
