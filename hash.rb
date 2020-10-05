bd8bafbbad3332930c69b67dd3777d07bb64a5e9
thedoodler <ramprakashsmail@gmail.com>
2020-10-05 20:51:19 +0530

    hashes update

--- hash.rb
+++ hash.rb
@@ -16,3 +16,11 @@ def iter_hash(hash)
         puts key,value
     end
 end
+
+
+# new key value pairs can be added as hash.store(key,value)
+
+hash.store("ron",15)
+
+#for altering the original hash we can use keep_if and delete_if as in arrays
+
